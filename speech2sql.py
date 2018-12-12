a1931f88d2e0759534177efbdf193209d2bc021c
Kojo Addaquay <kojoaddaquay@gmail.com>
2018-12-11 23:19:53 -0500

    Annotated model, included summary infographic for web page

--- speech2sql.py
+++ speech2sql.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import sqlite3
+from flask import g
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/experiment")
+def model():
+  return render_template('SQLmodel.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
